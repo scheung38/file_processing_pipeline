81d636f90b9101c39e353271834c5133d09f7509
scheung38 <sebastian_cheung@yahoo.com>
2021-09-09 22:39:26 +0100

    Working draft but incorrect order of RowNo

--- src/simple_pandera_validate.py
+++ src/simple_pandera_validate.py
@@ -16,42 +16,55 @@ ic(pycountry.countries)
  
 # Loading the data:
 # data = pd.read_csv('./train.csv', parse_dates=['Date'])
-data = pd.read_csv('src/train.csv')
+test_data = pd.read_csv('src/train.csv')
+
+# print(type(test_data)) // <class 'pandas.core.frame.DataFrame'>
+ic(test_data)
 
 # Taking a subsample
 # data_sample = data.sample(n=3, random_state = 999)
-data_sample = data.sample(n=10)
+data_sample = test_data.sample(n=10)
 
- 
-
+Company = ['GOOG-Google', 'AAPL-Apple', 'TSLA-Tesla', 'NVDA-Nvidia', 'AMZN-Amazon', 'INTC-Intel', 'MSFT-Microsoft', 'ADI-Analog Devices', ]
+Deals = ['Deals-1', 'Deals-2', 'Deals-3', 'Deals-4', 'Deals-5']
 # Defining the schema
 schema = pa.DataFrameSchema({
 
     # "Id" : pa.Column(pa.Int, nullable=False),
     # "Date" : pa.Column(pa.DateTime),
-    
-    "DealName" : pa.Column(pa.String, required=True, nullable=False),
+     
+
+    "DealName" : pa.Column(pa.String, checks=pa.Check.isin(
+                [deal for deal in list(Deals)]), required=True, nullable=False),
     
     "D1" : pa.Column(pa.Float64, required=True),
-    "D2" : pa.Column(pa.Float64, required=False),
-    "D3" : pa.Column(pa.Float64, required=False),
-    "D4" : pa.Column(pa.Float64, required=False),
-    "D5" : pa.Column(pa.Float64, required=False),
+    "D2" : pa.Column(pa.Float64, nullable=True, required=False),
+    "D3" : pa.Column(pa.Float64, nullable=True, required=False),
+    "D4" : pa.Column(pa.Float64, nullable=True, required=False),
+    "D5" : pa.Column(pa.Float64, nullable=True, required=False),
     
     "IsActive" : pa.Column(pa.Bool, nullable=True, required=False),
-   
-    "Country" : pa.Column(pa.String, checks=pa.Check.isin(
-                [country.alpha_2 for country in list(pycountry.countries)]), required=False),
-    "Currency" : pa.Column(pa.String, checks=pa.Check.isin(
-                [currency.alpha_3 for currency in list(pycountry.currencies)]), required=False),
 
-    "Company" : pa.Column(pa.String, required=True),
+    "CountryName" : pa.Column(pa.String, checks=pa.Check.isin(
+                [country.name for country in list(pycountry.countries)]), required=True),
+ 
+    "CountryCode" : pa.Column(pa.String, checks=pa.Check.isin(
+                [country.alpha_2 for country in list(pycountry.countries)]), required=True),
+
+    "CurrencyName" : pa.Column(pa.String, checks=pa.Check.isin(
+                [currency.name for currency in list(pycountry.currencies)]), required=True),
+
+    "CurrencyCode" : pa.Column(pa.String, checks=pa.Check.isin(
+                [currency.alpha_3 for currency in list(pycountry.currencies)]), required=True),
+
+    "Company" : pa.Column(pa.String, checks=pa.Check.isin(
+                [company for company in list(Company)]), required=True),
 
 })
 
 
-example = schema.example(size=3)
-ic(example)
+example_input = schema.example(size=10)
+ic(example_input)
 
 
 
@@ -60,7 +73,7 @@ transformed_schema = schema.add_columns({
     "RowNo" : pa.Column(pa.String),
     "AsOfDate" : pa.Column(pa.DateTime),
     "ProcessIdentifier" : pa.Column(pa.String, nullable=False),
-    "RowHash" : pa.Column(pa.Int)
+    "RowHash" : pa.Column(pa.Int,   )
 })
 
 
@@ -69,9 +82,8 @@ transformed_schema = schema.add_columns({
 df_input = schema.validate(data_sample)
 ic(df_input)
 
-
-
-
-# Validating the transformed data
-transformed_schema.validate(data)
+# Validating the transformed_data
+df_output = transformed_schema.example(size=10)
+ic(df_output)
+ 
 
